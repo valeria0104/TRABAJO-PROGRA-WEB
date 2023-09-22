8ec15bbec8c10e5dac00d17cdb2792a13520b7ea
Valeria <vegavaleriah1@gmail.com>
2023-09-21 22:27:01 -0500

    holibi

--- trabajofinal/src/pages/pantalla1.js
+++ trabajofinal/src/pages/pantalla1.js
@@ -2,16 +2,32 @@ import Link  from "next/link"
 const Index = () => {
  return (
     <>
-      <div id = "olis">
-        <tittle >Sistema de reserva de libros</tittle>
+
+      <div>
+        <p id="olis">Sistema de reserva de libros</p>
+        <form action="#" method="get">
+        <ul>
+            <li>Usuario o Correo: <br></br>
+                <input type="text" id="op1" name="n1" />
+            </li>
+            <li>Contraseña: <br></br>
+                <input type="text" id="op2" name="n2" />
+            </li>
+        </ul>
+        <input type="submit" value="Enviar"/>
+        <input type="reset" value="Limpiar"/>
+
+    </form>
+      
+
 
       </div>
-     
+  
 
 
 </>
- )
-}
+ )}
+
 export default Index
 
 
