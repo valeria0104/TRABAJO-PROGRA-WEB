0d679dcd6983b02ccbaadab45220a502ea68c3d5
Valeria <vegavaleriah1@gmail.com>
2023-09-24 17:21:18 -0500

    holiboli

--- trabajofinal/src/pages/pantalla1.js
+++ trabajofinal/src/pages/pantalla1.js
@@ -1,22 +1,35 @@
 import Link  from "next/link"
+
 const Index = () => {
  return (
     <>
   <div>
-   
+         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&display=swap" />
+
         <p id="olis">Sistema de reserva de libros</p>
        
         <form action="#" method="get">
         <ul>
-            <li>Usuario o Correo:
-                <input type="text" id="op1" name="n1" />
+            <li><label>Usuario o Correo: </label>
+                <input class="input-box" type="text" id="op1" name="n1" />
             </li>
-            <li>Contraseña:
-                <input type="text" id="op2" name="n2" />
+            <li><label>Contraseña:</label>
+            <div className="input-with-link">
+              <input className="input-box" type="text" id="op2" name="n2" />
+              <div className="forgot-password">
+                <Link href="/contraseña" className="password">
+                  Olvidé mi contraseña
+                </Link>
+              </div>
+            </div>
             </li>
-        </ul>
-        <input type="submit" value="Enviar" />
-        <input type="reset" value="Limpiar"/>
+            
+        </ul>    
+
+        <div className="buttons">
+        <input type="button" value="Registro usuario"/> <t></t>
+        <input type="button" value="Ingresar" className="login-button"/>
+        </div>
 
     </form>
 
