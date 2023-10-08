59771b2143301db07e794212ec0dde38aeb6aa6c
Valeria <vegavaleriah1@gmail.com>
2023-10-08 16:58:25 -0500

    fsdf

--- trabajofinal/src/pages/_app.js
+++ trabajofinal/src/pages/_app.js
@@ -2,6 +2,7 @@ import './css/estilo.css'
 import './css/estilo1.css'
 import './css/cuerpo2.css'
 import './css/colores.css'
+import './css/index.css'
 import { AppProps } from 'next/app'
 
 export default function MyApp({Component, pageProps }){
