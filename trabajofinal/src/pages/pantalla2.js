2d7c24ebf0aa8f50b60d088b11fc24468a739b87
AstridM15 <astridmucha6@gmail.com>
2023-11-18 23:06:45 -0500

    hghghfh

--- trabajofinal/src/pages/pantalla2.js
+++ trabajofinal/src/pages/pantalla2.js
@@ -3,15 +3,17 @@ import Head from "next/head";
 import Layout from './componentes/Layout.js';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from "next/router";
+import { useAuth } from './context/demo'; // Importa el contexto de autenticación
 import datos from './json/archivo.json';
-import { useRouter } from "next/router";
-import reservas from '../../public/reserva.json';
 
 function App() {
   const [reservas, setReservas] = useState([]);
   const [librosAMostrar, setLibrosAMostrar] = useState([]);
   const [librosMasPedidos, setLibrosMasPedidos] = useState([]);
-
+  const { state } = useAuth();
+  const user = state.user;
+  const welcomeMessage = user ? `Bienvenido, ${user.nombres}` : 'Bienvenido';
+  console.log('User:', user); 
   // Efecto para cargar las reservas al montar el componente
   useEffect(() => {
     const obtenerReservas = async () => {
@@ -29,6 +31,7 @@ function App() {
         data.forEach((libro) => {
           pedidosPorLibro[libro.titulo] = (pedidosPorLibro[libro.titulo] || 0) + 1;
         });
+        
 
         // Ordenar los libros por la cantidad de pedidos en orden descendente
         const librosMasPedidos = Object.keys(pedidosPorLibro)
@@ -48,8 +51,7 @@ function App() {
     obtenerReservas();
   }, []);
 
-  // Fusiona las reservas con los datos de los libros
-  const librosMasPedidos = fusionarDatos(reservas, datos);
+  
 
   return (
     <Layout 
@@ -70,6 +72,7 @@ function App() {
                   <div className="cuadrado_letras">
                     <p>
                       <a href="pantalla3.html">{libro.titulo}</a>
+                      <br/>
                       {libro.fechaReserva}
                     </p>
                   </div>
@@ -93,17 +96,17 @@ function App() {
                   <div className="cuadrado_letras">
                     <p>
                       <a href="pantalla3.html">{libro.titulo}</a>
+                      <br/>
                       {` Pedidos: ${libro.pedidos}`}
 
                     </p>
                   </div>
-                  <div id="imagen_page2">
-                    <div className="cuadrado_libro">
-                      <a >
-                      <img src={datos.find((d) => d.titulo === libro.titulo)['imagen-portada-url']} height="100px" /></a>
+                  <div className="cuadrado_libro">
+                    <a>
+                    <img src={datos.find((d) => d.titulo === libro.titulo)['imagen-portada-url']} height="100px" />
+                    </a>
                     </div>
                   </div>
-                </div>
               ))}
             </section>
           </div>
